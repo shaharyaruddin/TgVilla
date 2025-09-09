c54cb5f01495eba237da55b8202f9fa20c0fe3ba
sheraz-saqib <bhanakop@gmail.com>
2025-09-09 07:12:26 +0100

    checkout page push

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="w-16 h-16 flex items-center justify-center bg-cyan-400 rounded-full animate-spin shadow-lg">
-        <Image src="/icons/tg-logo.svg" alt="logo" width={60} height={60} className="object-contain" />
+      <div className="w-16 h-16 flex items-center justify-center bg-primary rounded-full animate-spin shadow-lg">
+        <Image src="/assets/logo/tg-logo-2.svg" alt="logo" width={60} height={60} className="object-contain" />
       </div>
     </div>
   );

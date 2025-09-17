07e64c1fefda9a084f3e2dade67bb4a2643b55e7
shaharyaruddin <sheheryark505@gmail.com>
2025-09-17 16:37:21 +0500

    test residence animation

--- src/components/home/welcome-villa/VillaSection.jsx
+++ src/components/home/welcome-villa/VillaSection.jsx
@@ -4,7 +4,7 @@ import WelcomeRight from "./widgets/WelcomeRight";
 
 const VillaSection = () => {
   return (
-    <div className="bg-[#E8E4D9] grid grid-cols-2 welcomeSection max-width w-full max-lg:min-h-[30rem]  max-[51.25em]:grid-cols-1">
+    <div className="bg-[#E8E4D9] grid grid-cols-2 welcomeSection1 max-width w-full max-lg:min-h-[30rem]  max-[51.25em]:grid-cols-1">
       {/* left */}
       <WelcomeLeft />
       {/* right */}

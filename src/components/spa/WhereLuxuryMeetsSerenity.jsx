b4550ec4c1f0efb5ff16b60199837e81da213993
sheraz-saqib <bhanakop@gmail.com>
2025-09-18 11:12:53 +0500

    animation fix

--- src/components/spa/WhereLuxuryMeetsSerenity.jsx
+++ src/components/spa/WhereLuxuryMeetsSerenity.jsx
@@ -123,7 +123,7 @@ const WhereLuxuryMeetsSerenity = () => {
         </div>
 
         {/* Bottom left */}
-        <div className="absolute aboutBottomLeft bottom-40 left-40 max-md:bottom-20 max-md:left-4 max-md:size-[130px] md:size-[130px] lg:size-[240px]">
+        <div className="absolute aboutBottomLeft bottom-40 left-40 max-md:bottom-20 max-sm:bottom-10 max-md:left-4 max-md:size-[130px] md:size-[130px] lg:size-[240px]">
           <Image
             className="rounded-xl object-cover"
             src="/images/spa/3.avif"
@@ -133,7 +133,7 @@ const WhereLuxuryMeetsSerenity = () => {
         </div>
 
         {/* Bottom right */}
-        <div className="absolute aboutBottomRight bottom-40 right-20 max-md:bottom-20 max-md:right-4 max-md:size-[130px] md:size-[130px] lg:size-[190px]">
+        <div className="absolute aboutBottomRight bottom-40 right-20 max-md:bottom-20 max-sm:bottom-10 max-md:right-4 max-md:size-[130px] md:size-[130px] lg:size-[190px]">
           <Image
             className="rounded-xl object-cover"
             src="/images/spa/4.avif"

265cd43859a4e210d855eb23fd16efdeef8c0421
shaharyaruddin <sheheryark505@gmail.com>
2025-09-18 13:52:17 +0500

    unommit beach section

--- src/components/spa/SpaLuxuryTreatments.jsx
+++ src/components/spa/SpaLuxuryTreatments.jsx
@@ -103,7 +103,7 @@ const SpaLuxuryTreatments = () => {
             ))}
           </div>
         </div>
-        <div className="mt-5 w-full flex">
+        <div className="mt-5 w-full flex justify-center md:justify-start">
           <Link href="/bookings">
             <button className="bg-app-yellow transform transition-all duration-300 hover:scale-105 w-[10rem] rounded-full px-4 py-2 font-medium hover:bg-app-yellow/90 text-black">
               Book Now
